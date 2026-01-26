9d3b770b5e678ffcaa6f2474d55105ac093620da
Vincent Brodin <vincent.brodin21@gmail.com>
2026-01-26 05:21:26 +0100

    Updated about me

--- src/main.ts
+++ src/main.ts
@@ -29,11 +29,11 @@ export const main_page = async () =>
             "I’m a systems development student with a focus on building high-performance tools and infrastructure. Most of my work involves finding ways to make software faster and more efficient.",
           ]),
           h("span", [
-            "Currently, my main project is blaise, a high-performance routing engine for public transit data. I generally prefer working in environments where I have explicit control over memory and execution, as I find it easier to build reliable tools when I know exactly what the machine is doing under the hood.",
+            "Currently, my main project is Blaise, a routing engine for public transit data (GTFS). Working on Blaise is really fun and getting to to squeeze every single drop of performance out the routing engine has been a real challange but one that has been extremly educational. Some of my major breakthroughs include moving from A* to RAPTOR which allows for non blocking paralelism, replacing hash maps with array lookups (both are O(n) but array lookup is just ptr+n*item_size vs a hash alogrithm) for the in-memory database, and eliminating heap allocations in inner loops by using preallocated buffers.",
           ]),
 
           h("span", [
-            "Outside of larger projects, I spend a lot of time building small tools to automate my own workflow, ranging from custom window management to monitor configurations. I aim to keep everything I build lightweight, fast, and out of the way.",
+            "Outside of larger projects, I spend a lot of time building small tools to automate my own workflow, ranging from custom window management to monitor configurations. One of my goals is to have as little friction in my day to day development, and to fufil that goal i like to build tools/applications that can automate that part of my workflow.",
           ]),
         ],
 
